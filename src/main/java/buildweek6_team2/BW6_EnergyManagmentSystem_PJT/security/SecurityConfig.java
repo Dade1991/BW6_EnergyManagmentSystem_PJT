b548b69fce3c569139e9fdf268bd61ac70b4ce44
Claudio9905 <cloudpost10@gmail.com>
2025-10-28 12:11:09 +0100

    Adds files on Security-Branch ( SecurityConfig / JWTTools / JWTFilter )

---
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // 1. Disabilitazione dell'autenticazione basata sul form di default di Spring Security
+        httpSecurity.formLogin(formLogin -> formLogin.disable());
+        // 2. Disattivazione della protezione CSRF
+        httpSecurity.csrf(csrf -> csrf.disable());
+        // 3. Disabilitazione delle sessioni
+        httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Configurazione di default per il collegamento di con il FRONT_END
+        httpSecurity.cors(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    // Configurazione Cors
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3001"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    // Bcrypt per la sicurezza delle password per gli utenti
+    @Bean
+    public PasswordEncoder getBcrypt(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+}
