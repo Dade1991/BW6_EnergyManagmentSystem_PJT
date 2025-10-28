9caed9ffa9c57e9d4a7d4b5d9b006cca261d2c6b
Dade1991 <dade91@msn.com>
2025-10-28 11:25:54 +0100

    First commit of the PJT

---
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/Bw6EnergyManagmentSystemPjtApplication.java
@@ -0,0 +1,13 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw6EnergyManagmentSystemPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw6EnergyManagmentSystemPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/Bw6EnergyManagmentSystemPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw6EnergyManagmentSystemPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
