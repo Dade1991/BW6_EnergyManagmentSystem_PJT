0dc2288714a348d11b9e32492106a9a1d15c1283
Antimo <antimo6@icloud.com>
2025-10-28 14:19:33 +0100

    Creating an Exception, Error DTOs and Handlers

---
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
