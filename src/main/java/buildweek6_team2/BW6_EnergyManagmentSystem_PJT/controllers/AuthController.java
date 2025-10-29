96851c8240ab6b6bc2d9ac85ddd69399e759729d
Claudio9905 <cloudpost10@gmail.com>
2025-10-29 12:20:28 +0100

    Adds AuthController and AuthService files

---
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/controllers/AuthController.java
@@ -0,0 +1,4 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT.controllers;
+
+public class AuthController {
+}
---
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/services/AuthService.java
@@ -0,0 +1,4 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT.services;
+
+public class AuthService {
+}
