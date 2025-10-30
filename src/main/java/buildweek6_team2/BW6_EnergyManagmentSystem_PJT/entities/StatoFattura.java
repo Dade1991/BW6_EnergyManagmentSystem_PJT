4a2595bcf2404b5166d904f788b7273101d0667c
Dade1991 <dade91@msn.com>
2025-10-30 22:34:56 +0100

    Minor changes to code relative to "Fattura"

--- src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/entities/StatoFattura.java
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/entities/StatoFattura.java
@@ -5,17 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "stato_fatture")
 @Data
 @NoArgsConstructor
 @ToString
 public class StatoFattura {
+    @OneToMany(mappedBy = "statoFattura")
+    List<Fattura> listaFatture = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_stato_fattura")
     private Long idStatoFattura;
-
     private String stato;
 
     public StatoFattura(String stato) {
--- src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/repositories/StatoFatturaRepository.java
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/repositories/StatoFatturaRepository.java
@@ -2,9 +2,11 @@ package buildweek6_team2.BW6_EnergyManagmentSystem_PJT.repositories;
 
 import buildweek6_team2.BW6_EnergyManagmentSystem_PJT.entities.StatoFattura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StatoFatturaRepository extends JpaRepository<StatoFattura, Long> {
     Optional<StatoFattura> findByStato(String stato);
 
