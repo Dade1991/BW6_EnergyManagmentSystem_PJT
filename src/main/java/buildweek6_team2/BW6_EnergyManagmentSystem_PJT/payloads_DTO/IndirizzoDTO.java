343ecbf727e7e7d190437f44ca4f57f792c6dbf4
Antimo <antimo6@icloud.com>
2025-10-30 12:33:02 +0100

    Creation of the address repository, service, DTO, and controller

---
+++ src/main/java/buildweek6_team2/BW6_EnergyManagmentSystem_PJT/payloads_DTO/IndirizzoDTO.java
@@ -0,0 +1,23 @@
+package buildweek6_team2.BW6_EnergyManagmentSystem_PJT.payloads_DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record IndirizzoDTO(
+        @NotBlank(message = "La via è obbligatoria")
+        @Size(min = 3, max = 30, message = "La via deve essere tra 3 e 30 caratteri")
+        String via,
+
+        @NotBlank(message = "Il civico è obbligatorio")
+        @Size(min = 1, max = 10, message = "Il civico deve essere tra 1 e 10 caratteri")
+        String civico,
+
+        @NotBlank(message = "La località è obbligatoria")
+        @Size(min = 2, max = 50, message = "La località deve essere tra 2 e 50 caratteri")
+        String localita,
+
+        @NotBlank(message = "Il CAP è obbligatorio")
+        @Size(min = 5, max = 5, message = "Il cap deve essere composto da 5 numeri")
+        Integer cap
+) {
+}
